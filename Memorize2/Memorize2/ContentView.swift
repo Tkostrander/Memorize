f41ff3b160bc33e66b5a65169d1e701d85679d4a
Thomas Ostrander <tkostrander@icloud.com>
2023-10-23 20:54:29 -0500

    add card and delete card

--- Memorize2/Memorize2/ContentView.swift
+++ Memorize2/Memorize2/ContentView.swift
@@ -13,15 +13,24 @@ struct ContentView: View {
     @State var cardCount: Int = 4
 
     var body: some View {
-        HStack {
-            ForEach(0..<cardCount, id: \.self) { index in
-                CardView(content: emojis[index])
+        VStack {
+            HStack {
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content: emojis[index])
+                }
             }
-            Button("Add") {
-                 cardCount += 1
+            .foregroundColor(.orange)
+            
+            HStack {
+                Button("Add Card") {
+                     cardCount += 1
+                }
+                Spacer()
+                Button("Delete Card") {
+                     cardCount -= 1
+                }
             }
         }
-            .foregroundColor(.orange)
             .padding()
     }
 }
