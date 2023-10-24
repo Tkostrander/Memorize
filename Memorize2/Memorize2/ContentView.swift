5d6fde9b76c18fd9cc68e2117afafe9fcc39e8d9
Thomas Ostrander <tkostrander@icloud.com>
2023-10-23 20:24:21 -0500

    @state var change

--- Memorize2/Memorize2/ContentView.swift
+++ Memorize2/Memorize2/ContentView.swift
@@ -8,17 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis = ["👻","🎃","💀","👺","🕸️","🐮","🐷","🐻"]
+    
+    @State var cardCount: Int = 4
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+            }
+            Button("Add") {
+                 cardCount += 1
+            }
         }
-        .padding()
+            .foregroundColor(.orange)
+            .padding()
     }
 }
+
+struct CardView: View {
+    
+    @State var isFaceUp = true
+    let content: String
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
 }
+
