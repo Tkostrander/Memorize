ada890b8142be0d2f3b9fa8d7a7fbd674bb5b468
Thomas Ostrander <tkostrander@icloud.com>
2023-10-25 20:36:28 -0500

    finished task 1 assignment 1

--- Memorize2/Memorize2/ContentView.swift
+++ Memorize2/Memorize2/ContentView.swift
@@ -14,25 +14,56 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            HStack {
-                ForEach(0..<cardCount, id: \.self) { index in
-                    CardView(content: emojis[index])
-                }
+            ScrollView {
+                cards
             }
-            .foregroundColor(.orange)
-            
-            HStack {
-                Button("Add Card") {
-                     cardCount += 1
-                }
-                Spacer()
-                Button("Delete Card") {
-                     cardCount -= 1
-                }
-            }
+            Spacer()
+            cardCountAdjusters
         }
             .padding()
     }
+    
+    
+    func cardCountAdjuster(by offset: Int, symbol: String) -> some View {
+        Button(action: {
+                cardCount += offset
+        }, label: {
+            Image(systemName: symbol)
+        })
+        .disabled(cardCount + offset < 1 || cardCount + offset > emojis.count)
+    }
+    
+    
+    var cardCountAdjusters: some View {
+        HStack {
+            cardAdder
+            Spacer()
+            cardRemover
+        }
+        .imageScale(.large)
+        .font(.largeTitle)
+    
+        
+    }
+    var cards: some View {
+        LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+            ForEach(0..<cardCount, id: \.self) { index in
+                CardView(content: emojis[index])
+                    .aspectRatio(2/3, contentMode: .fit)
+            }
+        }
+        .foregroundColor(.orange)
+        
+    }
+    
+    var cardRemover: some View {
+        cardCountAdjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+    }
+    
+    var cardAdder: some View {
+        cardCountAdjuster(by: 1, symbol: "rectangle.stack.badge.plus.fill")
+ }
+
 }
 
 struct CardView: View {
@@ -43,13 +74,14 @@ struct CardView: View {
     var body: some View {
         ZStack {
             let base = RoundedRectangle(cornerRadius: 12)
-            if isFaceUp {
+            Group {
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
                 Text(content).font(.largeTitle)
-            } else {
-                base
             }
+            .opacity(isFaceUp ? 1 : 0)
+            base.fill().opacity(isFaceUp ? 0 : 1)
+            
         }
         .onTapGesture {
             isFaceUp.toggle()
@@ -57,7 +89,7 @@ struct CardView: View {
     }
 }
 
-// I don know what is going on.
+
 
 
 
